741c65961ffb55829306e201323d234de9664809
Fyodor Egorov <89541604+S13gfried@users.noreply.github.com>
2023-02-12 17:36:49 +0300

    added first iteration code

--- main.cpp
+++ main.cpp
@@ -4,14 +4,53 @@
 
 #include <iostream>
 #include <stdio.h>
+#include <errno.h>
+#include <assert.h>
+
+#define WINDOW_WIDTH 640
+#define WINDOW_HEIGHT 480
+#define WINDOW_TITLE "a fucking triangle"
 
 int main(void)
 {
-    GLFWwindow* appWindow;
-
     //initialize GLFW
-    if(!glfwInit())
-        {
+    assert(glfwInit()); 
+    //initialize GLEW
+    assert(glewInit() == GLEW_OK);
 
-        }
+   //create window context
+    GLFWwindow* appWindow;
+ 
+    appWindow = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE, NULL, NULL);
+    if(!appWindow)
+    {
+        glfwTerminate();
+        assert(0); //goofy ahh
+    }
+
+    //select the window we've just initialized
+    glfwMakeContextCurrent(appWindow);
+
+    //the following loop runs until the user closes the window.
+    while(!glfwWindowShouldClose(appWindow))
+    {
+        //clear the window
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        //enter triangle mode
+        glBegin(GL_TRIANGLES);
+        //specify vertices
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(0.5f, -0.5f);
+        glVertex2f(0.0f, 0.5f);
+        glEnd();
+
+        glfwSwapBuffers(appWindow);
+
+        glfwPollEvents();
+    }
+
+    //terminate
+    glfwTerminate();
+    return 0;
 }
