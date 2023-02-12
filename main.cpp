a607596a4fb5812cd1e02e384982ba44c9502ca9
Fyodor Egorov <89541604+S13gfried@users.noreply.github.com>
2023-02-12 16:30:31 +0300

    INITIALIZE

---
+++ main.cpp
@@ -0,0 +1,17 @@
+// Graphic dependencies
+#include <libraries/include/GL/glew.h> //Provides ways to access hardware and get functions to in
+#include <libraries/include/GLFW/glfw3.h>
+
+#include <iostream>
+#include <stdio.h>
+
+int main(void)
+{
+    GLFWwindow* appWindow;
+
+    //initialize GLFW
+    if(!glfwInit())
+        {
+
+        }
+}
